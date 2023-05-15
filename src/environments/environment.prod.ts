7ca59162df43e0fdb3f7f315cd0b36edcbb4bca0
Daniel Chapa <chapadaniel479@gmail.com>
2023-05-15 09:36:45 -0500

    ss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,8 @@
-export const environment = {
-  production: true,
-  firebaseConfig:{
-    apiKey: "AIzaSyDkY-nR8kC_-EDSC4sjgScFtjr3BVkOOb0",
-    authDomain: "velasquezmiltong1insumo1.firebaseapp.com",
-    databaseURL: "https://velasquezmiltong1insumo1-default-rtdb.firebaseio.com",
-    projectId: "velasquezmiltong1insumo1",
-    storageBucket: "velasquezmiltong1insumo1.appspot.com",
-    messagingSenderId: "770265989698",
-    appId: "1:770265989698:web:4cb11766da01ffca3563db"
-  }
+export const firebaseConfig = {
+  apiKey: "AIzaSyCIW9IudrKquXfwt__7lRS-mYybLDsDLNM",
+  authDomain: "restaur-ac7bb.firebaseapp.com",
+  projectId: "restaur-ac7bb",
+  storageBucket: "restaur-ac7bb.appspot.com",
+  messagingSenderId: "936908591282",
+  appId: "1:936908591282:web:c4c1a477d46738e6ccb9ab"
 };
